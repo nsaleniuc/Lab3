9acb94efb6c7dd14c6d7f729507a15df011dd878
Nathan <nsaleniuc@yahoo.com>
2017-06-28 22:04:59 -0400

    added spacing formatter

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Formatter;
 import java.util.Scanner;
 
 public class Main {
@@ -5,19 +6,28 @@ public class Main {
     public static void main(String[] args) {
 
         int userNum;
+        String cont = "y";
+        int numberOfSpaces = 11;
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter an integer:");
-        userNum = scan.nextInt();
+        while (cont.equalsIgnoreCase("y")) {
+            System.out.print("Enter an integer:");
+            userNum = scan.nextInt();
+            Formatter fmt;
 
+            System.out.println("Number    Squared     Cubed");
+            System.out.println("======    =======     =====");
 
-        System.out.println("Number      Squared     Cubed");
-        System.out.println("======      =======     =====");
+            for (int i = 1; i <= userNum; i++) {
+                fmt = new Formatter();
+                fmt.format("%4d      %4d       %4d", (i), (i*i), (i*i*i));
+                System.out.println(fmt);
+            }
 
-        for (int i = 1; i <= userNum; i++) {
-            System.out.println(i + "            " + (i * i) +
-                    "          " + (i * i * i));
+            scan.nextLine();
+            System.out.println("Continue? (y/n)");
+            cont = scan.nextLine();
         }
 
     }
